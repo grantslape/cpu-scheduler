791f7b071039d3fc972a7153d72be6d9d12a11ac
Grant Slape <me@grantslape.com>
2018-11-02 19:31:23 -0500

    remove __repr__ from process

--- src/process.py
+++ src/process.py
@@ -50,16 +50,6 @@ class Process:
                str(self.run_time) + '","total_time":"' + str(self.total_time) + \
                '","used":"' + str(self.used) + '","completed_at":"' + str(self.completed_at) + '"}'
 
-    # def __repr__(self):
-    #     members = (self.id,
-    #                self.created_at,
-    #                self.start_at,
-    #                self.run_time,
-    #                self.total_time,
-    #                self.used,
-    #                self.completed_at)
-    #     return "{0},{1},{2},{3},{4},{5},{6}".format(*members)
-
     def set_completed(self, completed_at: Arrow) -> float:
         """
         Set process as completed
